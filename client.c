3e9cfdfcb13eabce0229e1532c67493f51d88d67
Minh Anh <112614452+tietbaothoa@users.noreply.github.com>
2025-07-28 16:28:07 +0700

    Add cmt client.c

--- client.c
+++ client.c
@@ -7,8 +7,11 @@ DWORD WINAPI recvThread(LPVOID lpParam) {
     char message_server[1024];
 
     while (1) {
+        // xoa tin nhan cu
         memset(message_server, 0, sizeof(message_server));
+        // nhan tin nhan tu server
         int read = recv(sockfd, message_server, sizeof(message_server), 0);
+        // dong ket noi neu read failed
         if (read <= 0) {
             printf("Disconnected from server.\n");
             closesocket(sockfd);
@@ -23,33 +26,37 @@ DWORD WINAPI recvThread(LPVOID lpParam) {
 
 int main() {
     WSADATA wsa;
+    // khoi tao socket
     if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
         printf("Failed to initialize WinSock\n");
         return 0;
     }
 
     struct sockaddr_in server_addr;
+    // tao socket su dung IPv4 va TCP
     SOCKET sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd == INVALID_SOCKET) {
         printf("FAILED WHILE set up on client");
         return 0;
     }
-
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(5000);
-    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-
+    // thiet lap dia chi server
+    server_addr.sin_family = AF_INET; //su dung IPv4
+    server_addr.sin_port = htons(5000); //lang nghe tren port 5000
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); // lang nghe tren ip 
+    // thiet lap ket noi den server
     if (connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
         printf("Connection failed\n");
         return 0;
     }
-
+    //tạo thread xu ly ham recvThread de nhan tin nhan ke ca khi client dang nhap tin nhan
     CreateThread(NULL, 0, recvThread, (LPVOID)&sockfd, 0, NULL);
 
     while (1) {
         char message_client[1024] = {0};
+        // nhap tin nhan
         fgets(message_client, sizeof(message_client), stdin);
         message_client[strcspn(message_client, "\n")] = '\0';
+        // gui tin nhan cho server
         int sent = send(sockfd, message_client, strlen(message_client), 0);
         if (sent <= 0) {
             printf("Failed while sending message.\n");
